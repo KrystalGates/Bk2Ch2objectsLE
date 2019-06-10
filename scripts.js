2a71993b90ca46f52782ec17f582d7be7c6172ac
krystal gates <krystal_sabrina@yahoo.com>
2019-06-10 08:43:42 -0500

    exercise complete

---
+++ scripts.js
@@ -0,0 +1,36 @@
+let car = {
+    make: "ford",
+    model: "mustang",
+    year: "2015",
+}
+console.log(car)
+
+let animalNames = ["Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", "Sharky"]
+console.log(animalNames)
+
+let sister = {
+    age: 25,
+    name: "Sheri",
+    hair: "brown",
+}
+
+let dad = {
+    age: 73,
+    name: "Ron",
+    hair: "bald",
+}
+
+let mom = {
+    age: 56,
+    name: "Juliann",
+    hair: "black",
+}
+
+let husband = {
+    age: 24,
+    name: "Christian",
+    hair: "dark blonde"
+}
+
+let familyMembers = [sister, dad, mom, husband]
+console.log(familyMembers)
